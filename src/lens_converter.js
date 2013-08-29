9135ca10c3f5a6c60076df65bf780cdbfe3f48c5
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-08-30 01:17:25 +0200

    Annotations.

--- src/lens_converter.js
+++ src/lens_converter.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _ = require("underscore");
 var Converter = null;
 var Converter = require("substance-converter");
 var NLMImporter = Converter.NLMImporter;
@@ -29,7 +30,7 @@ LensImporter.Prototype = function() {
   // TODO: this is experimental, and needs some experience from developing a more complex converter (e.g., for lens)
   this.show = function(state, nodes) {
     var doc = state.doc;
-    
+
     // Defaults to content
     function getView(viewName) {
       return doc.get(viewName || "content").nodes;
@@ -42,6 +43,54 @@ LensImporter.Prototype = function() {
 
       view.push(node.id);
     }
+  };
+
+  var _annotationTypes = {
+    "bold": "strong",
+    "italic": "emphasis",
+    "monospace": "code",
+    "sub": "subscript",
+    "sup": "superscript",
+    "underline": "underline",
+    "xref": ""
+  };
+
+  this.isAnnotation = function(type) {
+    return _annotationTypes[type] !== undefined;
+  };
+
+  this.createAnnotation = function(state, el, start, end) {
+    var type = el.tagName.toLowerCase();
+    var anno = {
+      path: _.last(state.stack).path,
+      range: [start, end],
+    };
+    if (type === "xref") {
+      var refType = el.getAttribute("ref-type");
+      var targetId = el.getAttribute("rid");
+      if (refType === "bibr") {
+        anno.type = "citation_reference";
+      } else if (refType === "fig" || refType === "table") {
+        anno.type = "figure_reference";
+      }
+      // 'supplementary-material', disp-formula
+      else {
+        console.log("Ignoring xref: ", refType, el);
+        return;
+      }
+      anno.target = targetId;
+    }
+    // Common annotations (e.g., emphasis)
+    else if (_annotationTypes[type] !== undefined) {
+      anno.type = _annotationTypes[type];
+    }
+    else {
+      console.log("Ignoring annotation: ", type, el);
+      return;
+    }
+    anno.id = state.nextId(anno.type);
+
+    state.annotations.push(anno);
   };
 };
 
