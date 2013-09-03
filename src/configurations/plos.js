79e4ea88a760c3370396fc63008228aa01eac76e
Michael Aufreiter <michael@substance.io>
2013-09-03 17:01:35 +0200

    PLOS figures.

--- src/configurations/plos.js
+++ src/configurations/plos.js
@@ -12,26 +12,15 @@ PLOSConfiguration.Prototype = function() {
     var graphic = figure.querySelector("graphic");
     var url = graphic.getAttribute("xlink:href");
 
-    // Example url to SVG: http://cdn.elifesciences.org/elife-articles/00768/svg/elife00768f001.svg
-    // Where are the images with that layout?
-  
-    console.log('URL', url);    
-    // url = [
-    //   "http://cdn.elifesciences.org/elife-articles/",
-    //   state.doc.id,
-    //   "/svg/",
-    //   url,
-    //   ".svg"
-    // ].join('');
-
-    // http://www.plosone.org/article/fetchObject.action?uri=info:doi/10.1371/journal.pone.0072727.g002&representation=PNG_I
-
-    // info:doi/10.1371/journal.pone.0072727.g001
-    // http://www.plosone.org/article/fetchObject.action?uri=info:doi/10.1371/journal.pone.0072727.g002&representation=PNG_I
+    url = [
+      "http://www.plosone.org/article/fetchObject.action?uri=",
+      url,
+      "&representation=PNG_M"
+    ].join('');
 
     return {
-      url: "http://t3.gstatic.com/images?q=tbn:ANd9GcSZzxvW5ZqgzZD-vBAmn5dDQvLLwS0dcSKh9_yrGaVo2DxqJ8NELg",
-      large_url: "http://t3.gstatic.com/images?q=tbn:ANd9GcSZzxvW5ZqgzZD-vBAmn5dDQvLLwS0dcSKh9_yrGaVo2DxqJ8NELg"
+      url: url,
+      large_url: url
     };
   };
 };
