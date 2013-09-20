b0b2fca5a6d660bafbc4e8643df40e11533fec88
Ivan Grubisic <ivan.grub@gmail.com>
2013-09-20 16:14:11 -0700

    debug

--- src/lens_converter.js
+++ src/lens_converter.js
@@ -971,8 +971,8 @@ LensImporter.Prototype = function() {
       }
     }    
     p1.children.push(text1);
-    console.log(JSON.stringify(par))
     p1.children.push(par.id);
+    console.log(JSON.stringify(p1))
     return p1.id;
   };
 
