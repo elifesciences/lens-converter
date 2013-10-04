ca967f2b6a5c18374b5207be59b68c20c8c8ac7c
Michael Aufreiter <michael@substance.io>
2013-10-04 02:15:32 +0200

    Implemented default resolvers for figure and video URL's.

--- src/configurations/default.js
+++ src/configurations/default.js
@@ -13,12 +13,34 @@ DefaultConfiguration.Prototype = function() {
     // Noop - override in your configuration
   };
 
+  // Default video resolver
+  // --------
+  // 
+
   this.enhanceVideo = function(state, node, element) {
-    // Noop - override in your configuration
+    var el = element.querySelector("media") || element;
+
+    // xlink:href example: elife00778v001.mov
+    var url = element.getAttribute("xlink:href");
+    // Just return absolute urls
+    if (url.match(/http:/)) {
+      node.url = url;
+      return;
+    } else {
+      var name = url.split(".")[0];
+      node.url = state.options.baseURL+name+".mp4";
+      node.url_ogv = state.options.baseURL+name+".ogv";
+      node.url_webm = state.options.baseURL+name+".webm";
+      node.poster = state.options.baseURL+name+".jpg";
+    }
   };
 
+  // Implements resloving of relative urls
   this.enhanceFigure = function(state, node, element) {
-    // Noop - override in your configuration
+    var graphic = element.querySelector("graphic");
+    var url = graphic.getAttribute("xlink:href");
+
+    node.url = this.resolveURL(state, url);
   };
 
   this.enhanceArticle = function(converter, state, article) {
@@ -32,8 +54,21 @@ DefaultConfiguration.Prototype = function() {
   this.resolveURL = function(url) {
     return url;
   };
+
+  // Default figure url resolver
+  // --------
+  // 
+  // For relative urls it uses the same basebath as the source XML
+
+  this.resolveURL = function(state, url) {
+    // Just return absolute urls
+    if (url.match(/http:/)) return url;
+    return [
+      state.options.baseURL,
+      url
+    ].join('');
+  };
 };
 
 DefaultConfiguration.prototype = new DefaultConfiguration.Prototype();
-
 module.exports = DefaultConfiguration;
