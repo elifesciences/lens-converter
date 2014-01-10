cee19b79cf2e71383420bd7b01df68576950e8c1
Ivan Grubisic <ivan.grub@gmail.com>
2014-01-09 17:40:10 -0800

    issue with funding statement

--- src/configurations/plos.js
+++ src/configurations/plos.js
@@ -376,10 +376,9 @@ PLOSConfiguration.Prototype = function() {
       doc.create(h1);
       nodes.push(h1.id);
       var child = util.dom.getChildren(fund)
-      console.log(child)
-      var par = converter.bodyNodes(state,child[0]);
+      var par = converter.bodyNodes(state, child[0]);
       console.log(par)
-      nodes.push(par[0].id);
+      //nodes.push(par[0].id);
     }
 
     // Get copyright and license information
